74e94c36f997bdbdaad0143751478018c1db129a
Nagaraj <124070128+Nagaraj02451@users.noreply.github.com>
2024-11-08 15:45:51 +0530

    Update Navbar1.jsx

--- frontend/src/Components/Navbar1.jsx
+++ frontend/src/Components/Navbar1.jsx
@@ -1,25 +1,26 @@
 import React, {useState} from 'react'
 import '../Styles/Navbar1.css'
-import Sidebar from './Sidebar';
+import Sidebar from './Sidebar/Sidebar';
 
 const Navbar1 = () => {
   const [isopen, setisopen] = useState(false);
   const toggle = () => {
     setisopen(!isopen);
   };
+
   return (
     <>
 
+{/* <Sidebar isopen={isopen} toggle={toggle} /> */}
 <Sidebar isopen={isopen} toggle={toggle} />
-
 
     <div className='nav1'>
         <div className='nav2'>
          <a href='site' className='nav1a'>OUR STAYS</a>
          <a href='contact' className='nav1a'>CONTACT US</a>
-         <a className='nav2a' href='/sidebar'  onClick={toggle}>
-            <img src='Img/nav1.svg'></img>
-         </a>
+       
+            <img onClick={toggle} src='Img/nav1.svg'></img>
+
         </div>
 
         <div className='nav3'>
@@ -29,9 +30,9 @@ const Navbar1 = () => {
         </div>
 
         <div className='nav2'> 
-        <a className='nav2a nav12aa' href='/sidebar'  onClick={toggle}>
-        <img src='Img/nav1.svg'></img>
-        </a>
+      
+        {/* <img  onClick={toggle} src='Img/nav1.svg'></img> */}
+   
          <a href='property' className='nav1a'>LIST YOUR PROPERTY</a>
          <a href='' className='nav1a'>+91 5235 2523 53</a>
        </div>
